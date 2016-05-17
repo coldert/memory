afc93dc0a32605aa1d707c5ec81dea0eba29a366
Robert Wictorzon <robert@wictorzon.com>
2016-05-17 13:46:31 +0200

    Moved settings to a struct

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 /* Project: Memory Game 2016-05-06 */
 
 /*
- * TODO: Refactor more. The whole init routine..? (if rounds = 0, if !gameOn ...)
  * TODO: Two players?
  * TODO: AI?
  * */
@@ -11,26 +10,28 @@
 
 #define MAX_CARDS 26 // A-Z
 
+// Define and declare a game settings object
+typedef struct gameSettings {
+    int gameOn, rounds, noOfCards, maxCards, width, height;
+} Game;
+Game currentGame = {1, 0, 8, MAX_CARDS, 0, 0};
+Game *game = &currentGame;
+
 // Main program
 int main(int argc, char **argv)
 {
     int i;
-    int gameOn = 1; // Keep track of if the game is still active
-    int rounds = 0;
-    int board_x = 0;
-    int board_y = 0;
     char board[MAX_CARDS * 2] = {0}; // Must hold two sets of MAX_CARDS
     int guess[4] = {-1, -1, -1, -1};
     char answer; // Used to hold y/n answers from the player
-    int noOfCards = 8;
     
-    while(gameOn)
+    while(game->gameOn)
     {
         // Initialize the board if new game
-        if (rounds == 0)
+        if (game->rounds == 0)
         {
             printf("\n### M E M O R Y ###\n\n");
-            printf("Current board size is %d pairs. Do you want to change the size (y/n)? ", noOfCards);
+            printf("Current board size is %d pairs. Do you want to change the size (y/n)? ", game->noOfCards);
 
             // Get answer from player
             scanf("%c", &answer);
@@ -38,45 +39,45 @@ int main(int argc, char **argv)
             // Let the player change number of cards
             if (answer == 'y')
             {
-                getNoOfCards(&noOfCards, MAX_CARDS);
+                getNoOfCards(&game->noOfCards, game->maxCards);
             }
             
-            calculateBoardSize(&board_x, &board_y, noOfCards);
+            calculateBoardSize(&game->width, &game->height, game->noOfCards);
 
-            init(board, board_x * board_y);
+            init(board, game->width * game->height);
         }
         
         // Present the board
-        drawBoard(board, guess, board_x, board_y);
+        drawBoard(board, guess, game->width, game->height);
         
         // Get input from player
         for (i = 0; i < 2; i++)
         {
-            pickCard(board, guess, board_x, board_y);
-            drawBoard(board, guess, board_x, board_y);
+            pickCard(board, guess, game->width, game->height);
+            drawBoard(board, guess, game->width, game->height);
         }
         
         // Correct guess..?
-        checkCards(board, guess, board_x, board_y);
+        checkCards(board, guess, game->width, game->height);
         
         // Prepare for next round
         clearGuess(guess);
-        rounds++;
+        game->rounds++;
 
         // See if the board is completely solved
-        gameOn = !checkGame(board, board_x, board_y);
+        game->gameOn = !checkGame(board, game->width, game->height);
         
-        if (!gameOn)
+        if (!game->gameOn)
         {
             char answer;
             // We have a winner!
-            printf("\n\nYou won in %d rounds!\n", rounds);
+            printf("\n\nYou won in %d rounds!\n", game->rounds);
             printf("Want to play again (y/n)? ");
             getchar(); // Catch lingering input...
             scanf("%c", &answer);
             if (answer == 'y')  {
-                gameOn = 1;
-                rounds = 0;
+                game->gameOn = 1;
+                game->rounds = 0;
             };
             getchar();
         }
